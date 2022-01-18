ecfc0b2d4b31f82d652a56d33cd3fde79ef69a1f
ChittoX <77602685+HARSHCHITTORA@users.noreply.github.com>
2022-01-18 19:20:39 +0530

    Add files via upload

---
+++ next greater element to left.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<long long>nge(vector<long long> a,int n)
+{
+	vector<long long>v;
+	stack<long long>s;
+	for(int i=0;i<n;i++)
+	{
+		if(s.size()==0)
+		{
+			v.push_back(-1);
+		}
+		else if(s.size()>0 && s.top()>a[i])
+		{
+			v.push_back(s.top());
+		}
+		else if(s.size()>0 && s.top()<=a[i])
+		{
+			while(s.size()>0 && s.top()<=a[i])
+			{
+				s.pop();
+			}
+			if(s.size()==0)
+			{
+				v.push_back(-1);
+			}
+			else
+			{
+				v.push_back(s.top());
+			}
+		 } 
+		 s.push(a[i]);
+	}
+	return v;
+}
+int main()
+{
+	int n;
+	cout<<"enter number of element"<<endl;
+	cin>>n;
+	vector<long long>a(n);
+	cout<<"enter element in an array"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	vector<long long> res=nge(a,n);
+	cout<<"nexg greater element to left"<<endl;
+	for(int i=0; i < res.size(); i++)
+std::cout << res.at(i) << ' ';
+}
